e32a2d1a7768af3d5eb21effff528e77cdf4ba3b
Nata test <verba16@yandex.ru>
2023-12-17 16:21:19 +0300

    first commit

---
+++ artifacts/static/precache-manifest.babad6d8625a1ef13087e54aa001cdb8.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd464c70c6b066311afedc9460506a26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "237934903289711a8c4d",
+    "url": "/static/css/2.1e6aa287.chunk.css"
+  },
+  {
+    "revision": "e3c863f1953362ad09d7",
+    "url": "/static/css/main.a91d8783.chunk.css"
+  },
+  {
+    "revision": "237934903289711a8c4d",
+    "url": "/static/js/2.29bb44d1.chunk.js"
+  },
+  {
+    "revision": "e3c863f1953362ad09d7",
+    "url": "/static/js/main.830e29ae.chunk.js"
+  },
+  {
+    "revision": "297f7b0a1f09964d78fd",
+    "url": "/static/js/runtime-main.1b6c401a.js"
+  },
+  {
+    "revision": "fa350215ac949f5882c976f9509deaf2",
+    "url": "/static/media/card.fa350215.jpg"
+  },
+  {
+    "revision": "636407d800da69b3307e875d4c59700b",
+    "url": "/static/media/icon_category-guard_m_black.636407d8.svg"
+  },
+  {
+    "revision": "7d477607ef5feffc1e719c0e43b6e0b7",
+    "url": "/static/media/icon_category-guard_m_white.7d477607.svg"
+  },
+  {
+    "revision": "24cd64ee35059a806850d8ebcf7a67b7",
+    "url": "/static/media/icon_category-guard_xl_black.24cd64ee.svg"
+  },
+  {
+    "revision": "54b495c10c79c8625592638996700c68",
+    "url": "/static/media/icon_category-guard_xl_white.54b495c1.svg"
+  },
+  {
+    "revision": "3d833d65bc4c4174228f05897bc0f59f",
+    "url": "/static/media/icon_category-guard_xxl_black.3d833d65.svg"
+  },
+  {
+    "revision": "42a1f3bdc941940f765c6ed2082e6f77",
+    "url": "/static/media/icon_category-guard_xxl_white.42a1f3bd.svg"
+  },
+  {
+    "revision": "d2d0aea6d0547d7e4b6598ea96090907",
+    "url": "/static/media/icon_check-indeterminate_m_black.d2d0aea6.svg"
+  },
+  {
+    "revision": "33e19089d7f43002d6b5cfaa3ac11e0c",
+    "url": "/static/media/icon_check-indeterminate_m_white.33e19089.svg"
+  },
+  {
+    "revision": "623ae07c4f543b96989a722a21470df7",
+    "url": "/static/media/icon_check-indeterminate_s_black.623ae07c.svg"
+  },
+  {
+    "revision": "cc03cad908acc89654c9fce69b5c3522",
+    "url": "/static/media/icon_check-indeterminate_s_white.cc03cad9.svg"
+  },
+  {
+    "revision": "9fdfa24f2ace08fd45da3821d1531479",
+    "url": "/static/media/icon_close_l_black.9fdfa24f.svg"
+  },
+  {
+    "revision": "e81304e1ba9b00f38571aa084a3b5cfc",
+    "url": "/static/media/icon_close_l_white.e81304e1.svg"
+  },
+  {
+    "revision": "88b9180ac58c35601d8425254044892c",
+    "url": "/static/media/icon_close_m_black.88b9180a.svg"
+  },
+  {
+    "revision": "5d2c2756ab278b3f84735376af0c0284",
+    "url": "/static/media/icon_close_m_white.5d2c2756.svg"
+  },
+  {
+    "revision": "af6676b2ef164647620b1e1ea84bb7c2",
+    "url": "/static/media/icon_close_s_black.af6676b2.svg"
+  },
+  {
+    "revision": "cd06f8fbca7234fa3a10a5bc12a42205",
+    "url": "/static/media/icon_close_s_white.cd06f8fb.svg"
+  },
+  {
+    "revision": "217cfbbc40165c8a3437e68dc59deca1",
+    "url": "/static/media/icon_close_xl_black.217cfbbc.svg"
+  },
+  {
+    "revision": "0f90c8e88a2a481e64ef797a08517890",
+    "url": "/static/media/icon_close_xl_white.0f90c8e8.svg"
+  },
+  {
+    "revision": "f5bc862ef625a947a5be6abc9d25c1ac",
+    "url": "/static/media/icon_close_xs_black.f5bc862e.svg"
+  },
+  {
+    "revision": "bca604a95e3a09c97db710136b900ac3",
+    "url": "/static/media/icon_close_xs_white.bca604a9.svg"
+  },
+  {
+    "revision": "67e19155c9965f09c43f2d65fac40a2f",
+    "url": "/static/media/icon_ok_l_black.67e19155.svg"
+  },
+  {
+    "revision": "16e0255abce547ec8f1d6682b5aab6d3",
+    "url": "/static/media/icon_ok_l_color.16e0255a.svg"
+  },
+  {
+    "revision": "ff21cf8aae841f38374fd54ca31c7f73",
+    "url": "/static/media/icon_ok_l_white.ff21cf8a.svg"
+  },
+  {
+    "revision": "aebdfe474a5b7aeb52dd4a7019b7c9af",
+    "url": "/static/media/icon_ok_m_black.aebdfe47.svg"
+  },
+  {
+    "revision": "04027aa1241ff7fd60516fe7b7ed6fb6",
+    "url": "/static/media/icon_ok_m_color.04027aa1.svg"
+  },
+  {
+    "revision": "5987da79b448a2dc500be3e122e9aea0",
+    "url": "/static/media/icon_ok_m_white.5987da79.svg"
+  },
+  {
+    "revision": "6a3cfe692c73fcdbe7d267e4638e236b",
+    "url": "/static/media/icon_ok_s_black.6a3cfe69.svg"
+  },
+  {
+    "revision": "9e62e77a915dae67c67a32f697413cda",
+    "url": "/static/media/icon_ok_s_color.9e62e77a.svg"
+  },
+  {
+    "revision": "c4e915931419bc22b9e9599aa596984f",
+    "url": "/static/media/icon_ok_s_white.c4e91593.svg"
+  },
+  {
+    "revision": "dc435f2f31cb5a111fff72f25ca9cb1f",
+    "url": "/static/media/icon_ok_xl_black.dc435f2f.svg"
+  },
+  {
+    "revision": "618c9a63d2b18b417e7b1e17e746f696",
+    "url": "/static/media/icon_ok_xl_color.618c9a63.svg"
+  },
+  {
+    "revision": "14781cbbd049bff7c21e72c0d865ef70",
+    "url": "/static/media/icon_ok_xl_white.14781cbb.svg"
+  },
+  {
+    "revision": "9986f739d58da6f730aa7032b01f96b8",
+    "url": "/static/media/icon_ok_xxl_black.9986f739.svg"
+  },
+  {
+    "revision": "9ce5d519e2085fc785fd1c36b5ba55f4",
+    "url": "/static/media/icon_ok_xxl_color.9ce5d519.svg"
+  },
+  {
+    "revision": "341f0c8caae7a247fb249e4de696696d",
+    "url": "/static/media/icon_ok_xxl_white.341f0c8c.svg"
+  },
+  {
+    "revision": "129d1ee19160150f3ebf86fefa427cd5",
+    "url": "/static/media/icon_ok_xxxl_color.129d1ee1.svg"
+  },
+  {
+    "revision": "7a7690f429c9835df99c6cd61a2c85b2",
+    "url": "/static/media/icon_tick_l_black.7a7690f4.svg"
+  },
+  {
+    "revision": "1278335f1baa6e0dced3b5b98f758b6f",
+    "url": "/static/media/icon_tick_l_white.1278335f.svg"
+  },
+  {
+    "revision": "d4efddc78eb12d4eac58008441d1dfc1",
+    "url": "/static/media/icon_tick_m_black.d4efddc7.svg"
+  },
+  {
+    "revision": "91d46d42020b5710afb237df4bbc016b",
+    "url": "/static/media/icon_tick_m_white.91d46d42.svg"
+  },
+  {
+    "revision": "f9bec6c98aaf812650aa3ee06b174fa9",
+    "url": "/static/media/icon_tick_s_black.f9bec6c9.svg"
+  },
+  {
+    "revision": "141f1b40f5876a09abab46afa6601ac5",
+    "url": "/static/media/icon_tick_s_white.141f1b40.svg"
+  },
+  {
+    "revision": "fbad21621cfacbf2c31cd3e2442ce803",
+    "url": "/static/media/icon_tick_xl_black.fbad2162.svg"
+  },
+  {
+    "revision": "28a2987facae5edb2777000a8b0aa13b",
+    "url": "/static/media/icon_tick_xl_white.28a2987f.svg"
+  },
+  {
+    "revision": "d6c16d674a1dabf49b91a0258652861f",
+    "url": "/static/media/icon_tick_xs_black.d6c16d67.svg"
+  },
+  {
+    "revision": "171191614d77238f6b0942cf64bb0d04",
+    "url": "/static/media/icon_tick_xs_white.17119161.svg"
+  },
+  {
+    "revision": "d43653694bd21afcc88d3fdb29b433c3",
+    "url": "/static/media/icon_tick_xxl_black.d4365369.svg"
+  },
+  {
+    "revision": "99350c87559edc433e0d5c91bf84279a",
+    "url": "/static/media/icon_tick_xxl_white.99350c87.svg"
+  }
+]);
---
+++ src/test/java/DebitCardOrderTest.java
@@ -0,0 +1,48 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DebitCardOrderTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void BeforeEach() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://0.0.0.0:7777");
+
+    }
+
+    @AfterEach
+    void AfterEach() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void ShouldSendFormIfAllCorrect() {
+driver.findElement(By.cssSelector("[data-test-id = 'name'] input")).sendKeys("Иван Иванович Иванов");
+driver.findElement(By.cssSelector("[data-test-id = 'phone'] input")).sendKeys("+79030000000");
+driver.findElement(By.cssSelector("[data-test-id='agreement]")).click();
+driver.findElement(By.cssSelector("button.button")).click();
+        String actualText = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = actualText;
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
+
